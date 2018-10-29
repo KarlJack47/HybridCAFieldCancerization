d8fc01e78d6000ce73d24864138200e6fb5b7086
Karl Deutscher <karldeutscher@gmail.com>
2018-10-29 16:07:09 -0600

    Multiply the influx and consumption terms by the time scale in the space update.

--- src/carcinogen_pde.h
+++ src/carcinogen_pde.h
@@ -17,7 +17,7 @@ __global__ void initialize(float *results, float ic, float bc, int Nx, int N, in
 	}
 }
 
-__global__ void space_step(float *sol, int cur_t, int next_t, int Nx, int maxT, double dt, double s, bool liquid,
+__global__ void space_step(float *sol, int cur_t, int next_t, int Nx, int maxT, float T_scale, double dt, double s, bool liquid,
 			   float influx, int num_cells_absorb, float amount_per_cell, bool *boundary_edge, Cell *cells,
 			   curandState_t* states) {
 	int row = threadIdx.x + blockIdx.x * blockDim.x;
@@ -44,12 +44,12 @@ __global__ void space_step(float *sol, int cur_t, int next_t, int Nx, int maxT, 
 			sol[next] = sol[cur] +
 				    s*(10*sol[cur-1] - 4*sol[cur+1] +
 				    14*sol[cur+2] - 6*sol[cur+3] + sol[cur+4] - 15*sol[cur]) +
-				    dt*(fmaxf(0, in) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, in) - cells[idx].consumption);
 		} else
 			sol[next] = sol[cur] +
 				    s*(10*sol[cur-1] -
 				    4*sol[cur+1] + 14*sol[cur+2] - 6*sol[cur+3] + sol[cur+4] - 15*sol[cur]) +
-				    dt*(fmaxf(0, influx - absorbed) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, influx - absorbed) - cells[idx].consumption);
 	} else if (idx == Nx-2) {
 		if (liquid == true) {
 			int in = 0;
@@ -57,12 +57,12 @@ __global__ void space_step(float *sol, int cur_t, int next_t, int Nx, int maxT, 
 			sol[next] = sol[cur] +
 				    s*(10*sol[cur] -
 				    15*sol[cur-1] - 4*sol[cur-2] + 14*sol[cur-3] - 6*sol[cur-4] + sol[cur-5]) +
-				    dt*(fmaxf(0, in) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, in) - cells[idx].consumption);
 		} else
 			sol[next] = sol[cur] +
 				    s*(10*sol[cur] -
 				    15*sol[cur-1] - 4*sol[cur-2] + 14*sol[cur-3] - 6*sol[cur-4] + sol[cur-5]) +
-				    dt*(fmaxf(0, influx - absorbed) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, influx - absorbed) - cells[idx].consumption);
 	} else if (idx != 0 && idx != Nx-1) {
 		if (liquid == true) {
 			int in = 0;
@@ -70,12 +70,12 @@ __global__ void space_step(float *sol, int cur_t, int next_t, int Nx, int maxT, 
 			sol[next] = sol[cur] +
 				    s*(16*sol[cur+1] + 16*sol[cur-1] -
 				    sol[cur+2] - sol[cur-2] - 30*sol[cur]) +
-				    dt*(fmaxf(0, in) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, in) - cells[idx].consumption);
 		} else
 			sol[next] = sol[cur] +
 				    s*(16*sol[cur+1] + 16*sol[cur-1] -
 				    sol[cur+2] - sol[cur-2] - 30*sol[cur]) +
-				    dt*(fmaxf(0, influx - absorbed) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, influx - absorbed) - cells[idx].consumption);
 	} else if (idx == 0 || idx == Nx-1) {
 		if (liquid == true) {
 			int in = 0;
@@ -83,12 +83,12 @@ __global__ void space_step(float *sol, int cur_t, int next_t, int Nx, int maxT, 
 			sol[next] = sol[cur] +
 				    s*(16*sol[(cur+1)%(Nx*maxT)] + 16*sol[abs((cur-1)%(Nx*maxT))] -
 				    sol[(cur+2)%(Nx*maxT)] - sol[abs((cur-2)%(Nx*maxT))] - 30*sol[cur]) +
-				    dt*(fmaxf(0, in) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, in) - cells[idx].consumption);
 		} else
 			sol[next] = sol[cur] +
 				    s*(16*sol[(cur+1)%(Nx*maxT)] + 16*sol[abs((cur-1)%(Nx*maxT))] -
 				    sol[(cur+2)%(Nx*maxT)] - sol[abs((cur-2)%(Nx*maxT))] - 30*sol[cur]) +
-				    dt*(fmaxf(0, influx - absorbed) - cells[idx].consumption);
+				    T_scale * dt * (fmaxf(0, influx - absorbed) - cells[idx].consumption);
 	}
 }
 
@@ -213,7 +213,7 @@ struct CarcinogenPDE {
 		for (int n = 0; n < maxT-1; n++) {
 			int next = (n+1)*Nx;
 			int cur = n*Nx;
-			space_step<<< blocks, threads >>>(sol, cur, next, Nx, maxT, dt, s, liquid, influx,
+			space_step<<< blocks, threads >>>(sol, cur, next, Nx, maxT, T_scale, dt, s, liquid, influx,
 							  num_cells_absorb, amount_per_cell, boundary_edge, cells, states);
 			CudaCheckError();
 			CudaSafeCall(cudaDeviceSynchronize());
