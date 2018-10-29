191ff43e0339d5ad3d3ae35bc978a56f0141da5e
Karl Deutscher <karldeutscher@gmail.com>
2018-10-29 14:15:50 -0600

    Added create_results.sh to the bin directory.

---
+++ bin/create_results.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Simulation Runner"
+echo ""
+
+cd output
+
+for i in {1..100}; do
+	echo "Running simulation $i"
+	mkdir $i
+	cd $i
+	touch $i.txt
+	../../main 0 > $i.txt
+	cd ..
+	echo "Done simulation $i"
+done
+
+exit 0
