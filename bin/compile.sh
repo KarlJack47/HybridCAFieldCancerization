0e8ec5dd22557156f6850b85e4f026c885acd7b1
Karl Deutscher <karldeutscher@gmail.com>
2018-10-31 18:48:35 -0600

    Removed debug options from compile command.

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-nvcc -m64 -arch=compute_61 -use_fast_math src/common/lodepng.o src/main.cu -o main -lGL -lglut -lm -lpng -g -G
-
+nvcc -m64 -arch=compute_61 -use_fast_math src/common/lodepng.o src/main.cu -o main -lGL -lglut -lm -lpng
 exit 0
