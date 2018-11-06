03cfe21e68301324bcd0fe5717ab098cbd9ff910
Karl Deutscher <karldeutscher@gmail.com>
2018-11-06 11:28:52 -0700

    Fixed solution of PDE not verifying BC by updating boundaries properly.

--- src/carcinogen_pde.h
+++ src/carcinogen_pde.h
@@ -17,7 +17,7 @@ __global__ void initialize(float *results, float ic, float bc, int Nx, int N, in
 	}
 }
 
-__global__ void space_step(float *sol, int cur_t, int next_t, float T_scale, double dt, double s, bool liquid,
+__global__ void space_step(float *sol, int cur_t, int next_t, int N, float bc, float T_scale, double dt, double s, bool liquid,
 			   double influx, int num_cells_absorb, double amount_per_cell, Cell *cells, int carcin) {
 	int col = threadIdx.x + blockIdx.x * blockDim.x;
 	int row = threadIdx.y + blockIdx.y * blockDim.y;
@@ -31,26 +31,28 @@ __global__ void space_step(float *sol, int cur_t, int next_t, float T_scale, dou
 		absorbed = 0.0f;
 	}
 
-	bool neigh_exist = false;
-	for (int i = 0; i < 8; i++) {
-		if (sol[cur_t+cells[idx].neighbourhood[i]] != 0) {
-			neigh_exist = true;
-			break;
+	if (!(row == 0 || row == N-1 || col == 0 || col == N-1)) {
+		bool neigh_exist = false;
+		for (int i = 0; i < 8; i++) {
+			if (sol[cur_t+cells[idx].neighbourhood[i]] != 0) {
+				neigh_exist = true;
+				break;
+			}
 		}
-	}
 
-	if ((liquid && neigh_exist) || !liquid)
-		atomicAdd(&absorbed, amount_per_cell);
-	__syncthreads();
+		if ((liquid && neigh_exist) || !liquid)
+			atomicAdd(&absorbed, amount_per_cell);
+		__syncthreads();
 
-	double in = 0.0f;
-	if ((liquid && neigh_exist) || !liquid) in = influx - absorbed;
-	sol[next] = sol[cur] +
-		    s*(4*sol[cur_t+cells[idx].neighbourhood[0]] + 4*sol[cur_t+cells[idx].neighbourhood[1]] +
-		    4*sol[cur_t+cells[idx].neighbourhood[2]] + 4*sol[cur_t+cells[idx].neighbourhood[3]] +
-		    sol[cur_t+cells[idx].neighbourhood[4]] + sol[cur_t+cells[idx].neighbourhood[5]] +
-		    sol[cur_t+cells[idx].neighbourhood[6]] + sol[cur_t+cells[idx].neighbourhood[7]] - 20*sol[cur]) +
-		    T_scale * dt * (fmaxf(0, in) - cells[idx].consumption[carcin]);
+		double in = 0.0f;
+		if ((liquid && neigh_exist) || !liquid) in = influx - absorbed;
+		sol[next] = sol[cur] +
+			    s*(4*sol[cur_t+cells[idx].neighbourhood[0]] + 4*sol[cur_t+cells[idx].neighbourhood[1]] +
+		    	    4*sol[cur_t+cells[idx].neighbourhood[2]] + 4*sol[cur_t+cells[idx].neighbourhood[3]] +
+		    	    sol[cur_t+cells[idx].neighbourhood[4]] + sol[cur_t+cells[idx].neighbourhood[5]] +
+		    	    sol[cur_t+cells[idx].neighbourhood[6]] + sol[cur_t+cells[idx].neighbourhood[7]] - 20*sol[cur]) +
+		    	    T_scale * dt * (fmaxf(0, in) - cells[idx].consumption[carcin]);
+	} else sol[next] = bc;
 }
 
 struct CarcinogenPDE {
@@ -129,7 +131,7 @@ struct CarcinogenPDE {
 			int num_cells_absorb = (int) (fabsf(ceilf(rand() % (Nx+1))) * diffusion);
 			double amount_per_cell = influx / (double) num_cells_absorb;
 
-			space_step<<< blocks, threads >>>(sol, cur, next, T_scale, dt, s, liquid, influx,
+			space_step<<< blocks, threads >>>(sol, cur, next, N, bc, T_scale, dt, s, liquid, influx,
 							  num_cells_absorb, amount_per_cell, cells, carcin_idx);
 			CudaCheckError();
 			CudaSafeCall(cudaDeviceSynchronize());
