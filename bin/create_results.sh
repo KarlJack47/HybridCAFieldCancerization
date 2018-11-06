f884f00ee922ef611a34de943a07b7f7fe40527b
Karl Deutscher <karldeutscher@gmail.com>
2018-11-05 17:02:05 -0700

    Modified bin/simulations.sh and bin/create_results.sh so that they could take options allowing running simulations of arbituary grid size, number of time steps and number of simulations.

--- bin/create_results.sh
+++ bin/create_results.sh
@@ -1,18 +1,71 @@
 #!/bin/bash
 
-echo "Simulation Runner"
-echo ""
-
-cd output
-
-for i in {1..100}; do
-	echo "Running simulation $i"
-	mkdir $i
-	cd $i
-	touch $i.txt
-	../../main > $i.txt
-	cd ..
-	echo "Done simulation $i"
+verbose=0
+num_sim=100
+display=0
+save=0
+max_time=200
+grid_size=512
+while getopts vdsn:t:g: option; do
+    case "${option}"
+    in
+        v) verbose=1;;
+        d) display=1;;
+        s) save=1;;
+	n) num_sim=${OPTARG};;
+        t) max_time=${OPTARG};;
+        g) grid_size=${OPTARG};;
+    esac
 done
+
+if [ $verbose -eq 1 ]; then
+    echo "Simulation Runner"
+    echo "Running $num_sim simulations with a grid size of $grid_size and $max_time time steps."
+    echo ""
+fi
+
+if [ $save -eq 1 ]; then
+    cd output
+fi
+
+out_file=output_$(date -d "today" +"%Y%m%d%H%M%S").txt
+for ((i=1; i <= num_sim; i++)); do
+    if [ $verbose -eq 1 ]; then
+        echo "Running simulation $i"
+    fi
+
+    if [ $save -eq 1 ]; then
+        mkdir $i
+        cd $i
+        touch $i.txt
+        if [ $verbose -eq 1 ]; then
+            ../../main $display $save $max_time $grid_size | tee $i.txt
+        else
+            ../../main $display $save $max_time $grid_size > $i.txt
+        fi
+        cd ..
+    else
+        echo $i >> $out_file
+        if [ $verbose -eq 1 ]; then
+            ./main $display $save $max_time $grid_size | tee -a $out_file
+        else
+            ./main $display $save $max_time $grid_size >> $out_file
+        fi
+    fi
+
+    if [ $verbose -eq 1 ]; then
+        echo "Done simulation $i"
+    fi
+done
+
+if [ $verbose -eq 1 ]; then
+    echo ""
+fi
+if [ $save -eq 1 ]; then
+    cd ..
+    bin/get_stats.sh
+else
+    bin/get_stats.sh $out_file
+fi
 
 exit 0
--- bin/simulations.sh
+++ bin/simulations.sh
@@ -1,14 +1,77 @@
 #!/bin/bash
 
-if [ -d output ]; then
-    mv output output_$(date -d "today" +"%Y%m%d%H%M%S")
-    mkdir output
+help=0
+verbose=0
+num_sim=100
+display=0
+save=0
+max_time=200
+grid_size=512
+while getopts hvdsn:t:g: option; do
+    case "${option}"
+    in
+        h) help=1;;
+        v) verbose=1;;
+        d) display=1;;
+        s) save=1;;
+        n) num_sim=${OPTARG};;
+        t) max_time=${OPTARG};;
+        g) grid_size=${OPTARG};;
+    esac
+done
+if [ $help -eq 1 ]; then
+    echo "Usage: $0 [OPTION]"
+    echo "Run Hybrid CA a certain number of times with a certain grid size and number of time steps."
+    echo ""
+    echo "Options:"
+    echo "  -h		prints out the information you are currently reading"
+    echo "  -v		if flag is used then the progress of the script will be displayed"
+    echo "  -d		whether to display the simulation as it is running"
+    echo "  -s		whether to save the time steps into a file and create an output video"
+    echo "  -n int	number of simulations to run, default is 100"
+    echo "  -t int	number of time steps, default is 200 (note simulation hasn't been verified for >200 steps)"
+    echo "  -g int	size of the CA grid, default is 512 (should be a power of 2 between 16 and 1024)"
+    exit 0
 fi
 
-if [ ! -d output ]; then
-    mkdir output
+if [ $save -eq 1 ]; then
+    if [ -d output ]; then
+        mv output output_$(date -d "today" +"%Y%m%d%H%M%S")
+        mkdir output
+    fi
+    if [ ! -d output ]; then
+        mkdir output
+    fi
 fi
 
-bin/create_results.sh 2> /dev/null
+if [ $verbose -eq 1 ]; then
+    if [ $save -eq 1 ]; then
+        if [ $display -eq 1 ]; then
+            bin/create_results.sh -v -d -s -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        else
+            bin/create_results.sh -v -s -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        fi
+    else
+        if [ $display -eq 1 ]; then
+            bin/create_results.sh -v -d -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        else
+            bin/create_results.sh -v -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        fi
+    fi
+else
+    if [ $save -eq 1 ]; then
+        if [ $display -eq 1 ]; then
+            bin/create_results.sh -d -s -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        else
+            bin/create_results.sh -s -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        fi
+    else
+        if [ $display -eq 1 ]; then
+            bin/create_results.sh -d -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        else
+            bin/create_results.sh -n $num_sim -t $max_time -g $grid_size 2> /dev/null
+        fi
+    fi
+fi
 
 exit 0
