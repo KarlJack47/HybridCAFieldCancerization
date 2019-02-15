95f86ec6cb00539dd937250bf77bd020bc664abd
Karl Deutscher <karldeutscher@gmail.com>
2019-02-14 17:07:21 -0700

    The command to compile the program now includes the necessary links to use GLFW as the window manager.

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nvcc -m64 -arch=compute_61 -use_fast_math src/common/lodepng.o src/main.cu -o main -lGL -lglut -lm -lpng
+nvcc -m64 -arch=compute_61 -use_fast_math `pkg-config --cflags glfw3` src/common/glad.o src/common/lodepng.o src/main.cu -o main `pkg-config --static --libs glfw3` -lm -lpng
 exit 0
