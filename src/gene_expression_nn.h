d41a1b5e72eb5ede10dfc901790b667ed1cfa162
Karl Deutscher <karldeutscher@gmail.com>
2019-02-22 01:31:39 -0700

    Reverted back to keeping the sign of the values in W_out so that gene relationships are maintained.

--- src/gene_expression_nn.h
+++ src/gene_expression_nn.h
@@ -142,23 +142,23 @@ struct GeneExpressionNN {
 				double incr = (curand_uniform_double(&states[cell]) * (RAND_INCR_B - RAND_INCR_A + 0.999999999f) + RAND_INCR_A) / 1000000000.0f;
 				W_out[M*n_output+M] += incr;
 				gene_expressions[M*2] += incr;
-				W_out[0] = W_out[0] - incr;
+				W_out[0] = fminf(0.0f, W_out[0] - incr);
 			} else {
 				double decr = (curand_uniform_double(&states[cell]) * (RAND_DECR_B - RAND_DECR_A + 0.999999999f) + RAND_DECR_A) / 1000000000.0f;
-				W_out[M*n_output+M] = W_out[M*n_output+M] - decr;
+				W_out[M*n_output+M] = fmaxf(W_out[M*n_output+M] - decr, 0.0f);
 				gene_expressions[M*2+1] += decr;
-				W_out[0] += decr;
+				W_out[0] = fminf(0.0f, W_out[0] + decr);
 			}
 		}
 		for (int i = 0; i < index_map[M*12]; i++) {
 			if (curand_uniform_double(&states[cell]) <= CHANCE_UPREG) {
 				double incr = (curand_uniform_double(&states[cell]) * (RAND_INCR_B - RAND_INCR_A + 0.999999999f) + RAND_INCR_A) / 1000000000.0f;
 				W_out[index_map[M*12+(i+1)]*n_output+index_map[M*12+(i+1)]] += incr;
-				W_out[index_map[M*12+(i+1)]*n_output] -= incr;
+				W_out[index_map[M*12+(i+1)]*n_output] = fmaxf(0.0f, W_out[index_map[M*12+(i+1)]*n_output] - incr);
 			} else {
 				double decr = (curand_uniform(&states[cell]) * (RAND_DECR_B - RAND_DECR_A + 0.999999999f) + RAND_DECR_A) / 1000000000.0f;
 				W_out[index_map[M*12+(i+1)]*n_output+index_map[M*12+(i+1)]] = fmaxf(0.0f, W_out[index_map[M*12+(i+1)]*n_output+index_map[M*12+(i+1)]] - decr);
-				W_out[index_map[M*12+(i+1)]*n_output] = W_out[index_map[M*12+(i+1)]*n_output]+decr;
+				W_out[index_map[M*12+(i+1)]*n_output] += decr;
 			}
 			if (index_map[(i+1)*12+(i+1)] == M) {
 				if (curand_uniform_double(&states[cell]) <= CHANCE_UPREG) {
@@ -166,7 +166,7 @@ struct GeneExpressionNN {
 					W_out[index_map[M*12+(i+1)]*n_output+M] += incr;
 				} else {
 					double decr = (curand_uniform_double(&states[cell]) * (RAND_DECR_B - RAND_DECR_A + 0.999999999f) + RAND_DECR_A) / 1000000000.0f;
-					W_out[index_map[M*12+(i+1)]*n_output+M] = W_out[index_map[M*12+(i+1)]*n_output+M] - decr;
+					W_out[index_map[M*12+(i+1)]*n_output+M] = fmaxf(0.0f, W_out[index_map[M*12+(i+1)]*n_output+M] - decr);
 				}
 			}
 		}
