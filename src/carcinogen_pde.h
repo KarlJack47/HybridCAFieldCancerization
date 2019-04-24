a2dd29dbb3eafb10a1c57aadea62c1d8bd3829cd
Karl Deutscher <karldeutscher@gmail.com>
2019-04-24 10:57:32 -0600

    The difference between the spread of liquid and non liquid was removed. In space_step the following changes occurred:   1. Input of the boolean value liquid was removed.   2. Name of consumption_per_cell was changed to outflux_per_cell.   3. The influx of the carcinogen no longer depends on whether a neighbouring cell already has the carcinogen or not. In the CarcinogenPDE constructor:   1. consum was changed to out   2. consumption_per_cell was changed to outflux_per_cell   3. The diffusion rate had been converted to hours which was unnecessary since this is already implied.   4. Input of the boolean liquid was removed and the variable itself   5. The Dirichlet BC was changed to be 0 from 1.

--- src/carcinogen_pde.h
+++ src/carcinogen_pde.h
@@ -19,35 +19,26 @@ __global__ void initialize(double *results, double ic, double bc, int Nx, int N,
 	}
 }
 
-__global__ void space_step(double *sol, int cur_t, int next_t, int N, double bc, double T_scale, double dt, double s, bool liquid,
-			   double influx_per_cell, double consumption_per_cell, Cell *cells, int carcin) {
+__global__ void space_step(double *sol, int cur_t, int next_t, int N, double bc, double T_scale, double dt, double s,
+			   double influx_per_cell, double outflux_per_cell, Cell *cells, int carcin) {
 	int col = threadIdx.x + blockIdx.x * blockDim.x;
 	int row = threadIdx.y + blockIdx.y * blockDim.y;
 	int idx = row + col * gridDim.x * blockDim.x;
 
 	int next = next_t + idx;
 	int cur = cur_t + idx;
-	int i;
 
 	if (row < N && col < N) {
 		if (!(row == 0 || row == N-1 || col == 0 || col == N-1)) {
-			bool neigh_exist = false;
-			for (i = 0; i < 8; i++) {
-				if (sol[cur_t+cells[idx].neighbourhood[i]] != 0) {
-					neigh_exist = true;
-					break;
-				}
-			}
-
-			double in = 0.0f; double consum = 0.0f;
-			if ((liquid && neigh_exist) || !liquid) in = influx_per_cell;
-			if (cells[idx].state != 6) consum = consumption_per_cell;
+			double in = 0.0f; double out = 0.0f;
+			in = influx_per_cell;
+			out = outflux_per_cell;
 			sol[next] = sol[cur] +
 				    s*(sol[cur_t+cells[idx].neighbourhood[0]] + sol[cur_t+cells[idx].neighbourhood[1]] +
 		    	   	    sol[cur_t+cells[idx].neighbourhood[2]] + sol[cur_t+cells[idx].neighbourhood[3]] +
 		    	    	    sol[cur_t+cells[idx].neighbourhood[4]] + sol[cur_t+cells[idx].neighbourhood[5]] +
 		    	    	    sol[cur_t+cells[idx].neighbourhood[6]] + sol[cur_t+cells[idx].neighbourhood[7]] - 8.0f*sol[cur]) +
-		    	    	    T_scale * dt * (in - consum);
+		    	    	    T_scale * dt * (in - out);
 		} else sol[next] = bc;
 	}
 }
@@ -62,7 +53,7 @@ struct CarcinogenPDE {
 	double ic;
 	double bc;
 	double diffusion;
-	double consumption_per_cell;
+	double outflux_per_cell;
 	double influx_per_cell;
 	int Nx;
 	int maxT;
@@ -73,22 +64,18 @@ struct CarcinogenPDE {
 	double *sol;
 	double *results;
 
-	CarcinogenPDE(int space_size, int num_timesteps, double diff, double consum, double in, bool liq, int idx, int dev) {
+	CarcinogenPDE(int space_size, int num_timesteps, double diff, double out, double in, int idx, int dev) {
 		device = dev;
 		N = space_size;
 		T = num_timesteps + 1;
 		T_scale = 16.0f*120.0f;
-		diffusion = diff*120.0f;
-		consumption_per_cell = consum;
+		diffusion = diff;
+		outflux_per_cell = out;
 		influx_per_cell = in;
 		dx = 1.0f / (double) N;
 		dt = 5e-4;
-		liquid = liq;
 		ic = 0.0f;
-		if (liquid == true)
-			bc = 1.0f;
-		else
-			bc = 0.0f;
+		bc = 0.0f;
 		Nx = N / dx;
 		maxT = 1 / dt;
 		s = (diffusion * (T_scale / (double) maxT) * dt) / (3 * dx * dx);
@@ -128,8 +115,8 @@ struct CarcinogenPDE {
 			int next = (n+1)*Nx;
 			int cur = n*Nx;
 
-			space_step<<< blocks, threads >>>(sol, cur, next, N, bc, T_scl, dt, s, liquid,
-							  influx_per_cell, consumption_per_cell, cells, carcin_idx);
+			space_step<<< blocks, threads >>>(sol, cur, next, N, bc, T_scl, dt, s,
+							  influx_per_cell, outflux_per_cell, cells, carcin_idx);
 			CudaCheckError();
 			CudaSafeCall(cudaDeviceSynchronize());
 		}
