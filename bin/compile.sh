50a27a622304beca79553d0dbdb9d2d4aca37d18
Karl Deutscher <karldeutscher@gmail.com>
2019-06-24 12:13:40 -0600

    Fixed issue causing openmp to not work. The parallel code wasn't running in parallel at all.

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nvcc -m64 -arch=compute_61 -use_fast_math `pkg-config --cflags glfw3` src/common/glad.o src/common/lodepng.o src/main.cu -o main `pkg-config --static --libs glfw3` -lm -lpng
+nvcc -m64 -arch=compute_61 -use_fast_math `pkg-config --cflags glfw3` src/common/glad.o src/common/lodepng.o src/main.cu -o main `pkg-config --static --libs glfw3` -lm -lpng -Xcompiler -fopenmp -lgomp
 exit 0
