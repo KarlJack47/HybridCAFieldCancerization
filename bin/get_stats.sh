fc6fb2a4b45483930173f2d9b50467161a05ff1b
Karl Deutscher <karldeutscher@gmail.com>
2020-06-08 21:25:36 -0600

    1. Average is now done with float division instead of integer. 2. Situation where there is just one simulation is fixed. 3. Excision is taken care of properly now.

--- bin/get_stats.sh
+++ bin/get_stats.sh
@@ -5,16 +5,24 @@ if [ ! $# -eq 1 ]; then
 fi
 
 input=$1/*/*.txt
+totalSim=$(grep "The CA will run for" $input | uniq | wc -l)
 delim='/'
+delim1='('
 posSimNum=2
 posVal=3
+posVal1=1
 exprStr=('first CSC' 'first TC' 'TC recurred')
-cellType=('CSC' 'TC')
+cellType=('CSC' 'TC' 'TC')
 formStr=('form' 'form' 'reform')
 totalStr=('simulations' 'simulations' 'excisions')
 
 for i in {0..2}; do
-    totalVals=$(grep "${exprStr[$i]}" $input | cut -f 1 -d '(' | uniq | wc -l)
+    if [ $i -eq 2 ]; then
+        delim1=' '
+        posVal1=1-6
+    fi
+
+    totalVals=$(grep "${exprStr[$i]}" $input | cut -f $posVal1 -d "$delim1" | wc -l)
 
     if [ $totalVals -eq 0 ]; then
         if [ ! $i -eq 2 ]; then
@@ -25,20 +33,29 @@ for i in {0..2}; do
         continue;
     fi
 
-    simInfo=($(grep "${exprStr[$i]}" $input | cut -f 1 -d '(' | uniq |\
+    simInfo=($(grep "${exprStr[$i]}" $input | cut -f $posVal1 -d "$delim1" |\
              sort -n -t "$delim" -k $posSimNum |\
              sed -e 's/[^0-9/ ]//g' | cut -d "$delim" -f $posVal))
-
-    if [ $totalVals -eq 1 ]; then
-        echo 'The first' ${cellType[$i]} 'formed at time step'\
-             ${simInfo[0]} 'for the simulation.'
-        continue;
+    if [ $totalSim -eq 1 ]; then
+        simInfo=(1 ${simInfo[@]})
+        for j in {1..1000}; do
+            if [ $j -eq $totalVals ]; then
+                break;
+            fi
+            #limit=$((2*$j+$j))
+            limit=$((2*$j))
+            simInfo=(${simInfo[@]:0:$limit} 1 ${simInfo[@]:$limit})
+        done
     fi
+
     simNum=${simInfo[0]}
     count=0
     total=0
+    total1=0
     min=${simInfo[1]}
+    min1=${simInfo[1]}
     max=${simInfo[1]}
+    max1=${simInfo[1]}
     for idx in ${!simInfo[@]}; do
         if [ $(($idx % 2)) -eq 0 ]; then
             if [ ! $i -eq 2 ]; then
@@ -46,7 +63,7 @@ for i in {0..2}; do
                 continue;
             fi
             if [ ! $simNum -eq ${simInfo[$idx]} ]; then
-                avg=$total/$count
+                avg=$(bc -l <<< "scale=6; $total1/$count")
                 echo 'On average it took' $avg 'time steps for a TC to'\
                      'reform for simulation' $simNum 'after' $count 'excisions.'
                 echo 'The minimum number of time steps for a TC to reform for'\
@@ -56,9 +73,9 @@ for i in {0..2}; do
 
                 simNum=${simInfo[$idx]}
                 count=0
-                total=0
-                min=${simInfo[$(($idx+1))]}
-                max=${simInfo[$(($idx+1))]}
+                total1=0
+                min1=${simInfo[$(($idx+1))]}
+                max1=${simInfo[$(($idx+1))]}
             fi
             continue;
         fi
@@ -69,17 +86,34 @@ for i in {0..2}; do
         fi
 
         total=$(($total+${simInfo[$idx]}))
+        total1=$(($total1+${simInfo[$idx]}))
         if [ ${simInfo[$idx]} -lt $min ]; then
             min=${simInfo[$idx]}
+        fi
+        if [ ${simInfo[$idx]} -lt $min1 ]; then
+            min1=${simInfo[$idx]}
         fi
         if [ ${simInfo[$idx]} -gt $max ]; then
             max=${simInfo[$idx]}
         fi
+        if [ ${simInfo[$idx]} -gt $max1 ]; then
+            max1=${simInfo[$idx]}
+        fi
         if [ $i -eq 2 ]; then
             count=$(($count+1))
         fi
+
+        if [[ $i -eq 2 && $idx -eq $((2*totalVals-1)) ]]; then
+            avg=$(bc -l <<< "scale=6; $total1/$count")
+            echo 'On average it took' $avg 'time steps for a TC to'\
+                 'reform for simulation' $simNum 'after' $count 'excisions.'
+            echo 'The minimum number of time steps for a TC to reform for'\
+                 'simulation' $simNum 'after' $count 'excisions was' $min'.'
+            echo 'The maximum number of time steps for a TC to reform for'\
+                 'simulation' $simNum 'after' $count 'excisions was' $max'.'
+       fi
     done
-    avg=$(($total/$totalVals))
+    avg=$(bc -l <<< "scale=6; $total/$totalVals")
 
     echo 'On average it took' $avg 'time steps for the first' ${cellType[$i]}\
          'to' ${formStr[$i]} 'for the' $totalVals ${totalStr[$i]}'.'
