7e470494837c3d2dfef7ba21ac24fd954b0726d6
Karl Deutscher <karldeutscher@gmail.com>
2020-06-18 18:06:52 -0600

    1. Added a link to the lz4 library so that the cell data could be compressed.

--- bin/compile.sh
+++ bin/compile.sh
@@ -2,6 +2,6 @@
 
 nvcc -m64 -arch=compute_61 -rdc=true -use_fast_math `pkg-config --cflags glfw3`\
   common/glad.o src/main.cu -o main `pkg-config --static --libs glfw3`\
-  -lm -lturbojpeg -Xcompiler -fopenmp -lgomp
+  -lm -lturbojpeg -llz4 -Xcompiler -fopenmp -lgomp
 
 exit 0
