342fe664d5ba712670ae9811eb11213b00a7401b
Karl Deutscher <karldeutscher@gmail.com>
2020-06-18 18:51:21 -0600

    1. Changes to anim_gpu_ca:   a) new variables:        i) radius: radius of excision specified by the user       ii) centerX, centerY: center of excision specified by the user by clicking                             in the gui      iii) numTC: keeps track of the number of tumour cells in the grid       iv) countData: used to store the count of various parameters        v) rTC: radius of excision calculated by the automated excision process       vi) tcX, tcY: center of excision calculated by the automated excision process      vii) excisionPerformed: used to check if an excision was performed     viii) streamsExcise: cudaStreams used when checking if a TC has recurred for                          each excision   b) save_cell_data_to_file is called to save all of the cell data to file for      data processing and possibly initializing the CA (future)   c) save_data is used to save counts of the following:       i) number of cells of each state over time      ii) number of cells successfully doing each phenotypic action over time     iii) number of cells that are positively mutated for each gene over time      iv) number of cells for each state successfully doing each phenotypic action       v) number of cells for each state that are  positively mutated for each gene   d) Fixed issue where if an excision was performed before a TC formed or after      another exicision before a TC recurred in the area of excision it wouldn't      track recurrence for that exicision   e) check_CSC_or_TC_formed now takes into account when an excision doesn't      remove all the tumour cells but instead just some of them and tracks for      new TC only occurring in that region   f) Due to the fact that now after excision there might still exist tumour      cells CA::timeTCAlive must be incremented whenever numTC != 0 and set to      zero when numTC = 0.   g) if CA::maxTTCAlive is set then everytime the modulo of CA::timeTCAlive is 0      an exicision is attempted   h) set_excision_circle calculates the radius of exicision and its center   i) excision: excises all the cells in a specified circle   j) Still if CA::perfectExcision is specified the same excision process as      before is performed   k) Removed the count variable as it was used for debugging 2. For anim_gpu_ca, anim_gpu_genes, anim_gpu_carcin, and anim_gpu_cell the    inputted cudaStream was removed because it was redundent and couldn't be used    to speed things up 3. In anim_gpu_timer_and_saver logic was fixed up so that all the timers start    and stop properly and the video saving is done when the windows or the    maximum number of time steps is achieved.

--- headers/anim_functions.h
+++ headers/anim_functions.h
@@ -3,24 +3,29 @@
 
 void anim_gpu_ca(uchar4* outputBitmap, unsigned dim, CA *ca,
                  unsigned ticks, bool display, bool paused, bool excise,
-                 bool windowsShouldClose, cudaStream_t stream)
+                 unsigned radius, unsigned centerX, unsigned centerY,
+                 bool *windowsShouldClose)
 {
-    unsigned i, *count;
+    unsigned i, j, *numTC, *countData, *rTC, numCells;
+    int *tcX, *tcY;
+    bool excisionPerformed = false;
     dim3 blocks(NBLOCKS(ca->gridSize, ca->blockSize),
                 NBLOCKS(ca->gridSize, ca->blockSize));
     dim3 threads(ca->blockSize, ca->blockSize);
-    cudaStream_t streams[2+ca->nCarcin];
-
-    for (i = 0; i < ca->nCarcin+2; i++)
-        CudaSafeCall(cudaStreamCreate(&streams[i]));
+    cudaStream_t streams[ca->nCarcin+2], *streamsExcise;
 
     if (ticks <= ca->maxT) {
+        for (i = 0; i < ca->nCarcin+2; i++)
+            CudaSafeCall(cudaStreamCreate(&streams[i]));
         if (ticks == 0 && (display || ca->save)) {
-            display_ca<<< blocks, threads, 0, stream >>>(
+            display_ca<<< blocks, threads, 0, streams[0] >>>(
                 outputBitmap, ca->newGrid, ca->gridSize,
                 ca->cellSize, dim, ca->stateColors
             );
             CudaCheckError();
+            if (!paused)
+                save_cell_data_to_file(ca, ticks, blocks, threads, streams[1]);
+            CudaSafeCall(cudaStreamSynchronize(streams[0]));
         } else if (!paused) {
             mutate_grid<<< blocks, threads, 0, streams[0] >>>(
                 ca->prevGrid, ca->gridSize, ca->NN, ca->pdes, ticks
@@ -29,66 +34,139 @@ void anim_gpu_ca(uchar4* outputBitmap, unsigned dim, CA *ca,
             CudaSafeCall(cudaStreamSynchronize(streams[0]));
 
             for (i = 0; i < ca->nCarcin; i++)
-                ca->pdes[i].time_step(ca->cellCycleLen * ticks,
-                                      ca->cellVolume, ca->blockSize,
-                                      streams[i+2]);
+                ca->pdes[i].time_step(ticks, ca->blockSize, &streams[i+2]);
 
             cells_gpu_to_gpu_copy<<< blocks, threads, 0, streams[0] >>>(
                 ca->prevGrid, ca->newGrid, ca->gridSize, ca->nGenes
             );
             CudaCheckError();
 
-            cudaMallocManaged((void**)&count, 12*sizeof(unsigned));
-            memset(count, 0, 12*sizeof(unsigned));
+            cudaMallocManaged((void**)&countData,
+                              (3*ca->nStates+4)*sizeof(unsigned));
+            memset(countData, 0, (3*ca->nStates+4)*sizeof(unsigned));
             rule<<< blocks, threads, 0, streams[0] >>>(
                 ca->newGrid, ca->prevGrid, ca->gridSize,
-                ca->nGenes, ticks, count, false
+                ca->nGenes, ticks, countData, false
             );
             CudaCheckError();
             rule<<< blocks, threads, 0, streams[0] >>>(
                 ca->newGrid, ca->prevGrid, ca->gridSize,
-                ca->nGenes, ticks, count, true
+                ca->nGenes, ticks, countData, true
             );
             CudaCheckError();
             CudaSafeCall(cudaStreamSynchronize(streams[0]));
 
-            check_CSC_or_TC_formed<<< blocks, threads, 0, streams[0] >>>(
-                ca->newGrid, ca->prevGrid, ca->gridSize, ticks,
-                ca->cscFormed, ca->tcFormed, ca->exciseCount, ca->timeTCDead
-            );
-            CudaCheckError();
+            for (i = 0; i < 4; i++) {
+                save_data(ca->countFiles[i+7], ca->headerCount, ticks,
+                          countData[i], 0, 0, 0);
+                for (j = 0; j < ca->nStates; j++) {
+                    if (j == EMPTY || (i == DIFF && (j == NC || j == MNC || j == TC)))
+                        continue;
+                    if (i != DIFF)
+                        save_data(ca->countFiles[j*3+(ca->nGenes+11)+i],
+                                  ca->headerCount, ticks, countData[(j*3+4)+i],
+                                  0, 0, 0);
+                    else
+                        save_data(ca->countFiles[j+(3*ca->nStates+ca->nGenes+6)],
+                                  ca->headerCount, ticks,
+                                  countData[(3*ca->nStates-1)+j], 0, 0, 0);
+                }
+            }
+            CudaSafeCall(cudaFree(countData)); countData = NULL;
 
-            update_states<<< blocks, threads, 0, streams[1] >>>(
+            CudaSafeCall(cudaMallocManaged((void**)&numTC,
+                                           sizeof(unsigned)));
+            *numTC = 0;
+            streamsExcise = (cudaStream_t*)malloc((ca->exciseCount+1)*sizeof(cudaStream_t));
+            for (i = 0; i < ca->exciseCount+1; i++) {
+                CudaSafeCall(cudaStreamCreate(&streamsExcise[i]));
+                if (i != 0 && ca->tcFormed[i]) continue;
+                check_CSC_or_TC_formed<<< blocks, threads, 0, streamsExcise[i] >>>(
+                    ca->newGrid, ca->prevGrid, ca->gridSize, ticks, ca->cscFormed,
+                    ca->tcFormed, i, ca->timeTCDead, ca->perfectExcision,
+                    ca->radius, ca->centerX, ca->centerY, numTC
+                );
+                CudaCheckError();
+            }
+
+            update_states<<< blocks, threads, 0, streams[0] >>>(
                 ca->newGrid, ca->gridSize, ca->nGenes
             );
             CudaCheckError();
 
-            CudaSafeCall(cudaStreamSynchronize(streams[0]));
-            CudaSafeCall(cudaStreamSynchronize(streams[1]));
+            for (i = 0; i < ca->exciseCount+1; i++) {
+                CudaSafeCall(cudaStreamSynchronize(streamsExcise[i]));
+                CudaSafeCall(cudaStreamDestroy(streamsExcise[i]));
+            }
+            free(streamsExcise); streamsExcise = NULL;
 
-            if (ca->tcFormed[ca->exciseCount]) ca->timeTCAlive++;
-            else ca->timeTCDead++;
+            if (*numTC != 0) ca->timeTCAlive++;
+            else ca->timeTCAlive = 0;
 
-            if (ca->exciseCount <= ca->maxExcise
-             && ca->tcFormed[ca->exciseCount]) {
-                if (excise || ca->timeTCAlive == ca->maxTTCAlive+1) {
+            for (i = 0; i < ca->exciseCount+1; i++)
+                if (!ca->tcFormed[i])
+                    ca->timeTCDead[i]++;
+
+            if ((excise || ca->timeTCAlive % ca->maxTTCAlive == 0)
+             && ca->exciseCount == ca->maxExcise)
+                printf("The maximum number of excisions have been performed.\n");
+
+            if (ca->exciseCount < ca->maxExcise
+             && (excise || ca->timeTCAlive % ca->maxTTCAlive == 0)) {
+                if (!ca->perfectExcision) {
+                    CudaSafeCall(cudaMallocManaged((void**)&rTC,
+                                                   sizeof(unsigned)));
+                    *rTC = 0;
+                    CudaSafeCall(cudaMallocManaged((void**)&tcX, sizeof(int)));
+                    CudaSafeCall(cudaMallocManaged((void**)&tcY, sizeof(int)));
+                    *tcX = -1; *tcY = -1;
+                    if (radius == 0 && *numTC != 0) {
+                        set_excision_circle<<< blocks, threads, 0, streams[0] >>>(
+                            ca->newGrid, ca->gridSize, rTC, tcX, tcY
+                        );
+                        CudaCheckError();
+                        CudaSafeCall(cudaStreamSynchronize(streams[0]));
+                    } else if (radius != 0) {
+                        *rTC = radius; *tcX = centerX; *tcY = centerY;
+                    }
+                    if (*rTC != 0) {
+                        excision<<< blocks, threads, 0, streams[0] >>>(
+                            ca->newGrid, ca->gridSize, ca->nGenes,
+                            *rTC, *tcX, *tcY, numTC
+                        );
+                        CudaCheckError();
+                        CudaSafeCall(cudaStreamSynchronize(streams[0]));
+                        printf("Radius of excision: %d, center: (%d, %d)\n",
+                               *rTC, *tcX, *tcY);
+
+                        excisionPerformed = true;
+                        ca->radius[ca->exciseCount+1] = *rTC;
+                        ca->centerX[ca->exciseCount+1] = *tcX;
+                        ca->centerY[ca->exciseCount+1] = *tcY;
+                        if (*numTC == 0) ca->timeTCAlive = 0;
+                    }
+                    CudaSafeCall(cudaFree(rTC)); rTC = NULL;
+                    CudaSafeCall(cudaFree(tcX)); tcX = NULL;
+                    CudaSafeCall(cudaFree(tcY)); tcY = NULL;
+                } else if (ca->perfectExcision && ca->tcFormed[ca->exciseCount]) {
                     tumour_excision<<< blocks, threads, 0, streams[0] >>>(
                         ca->newGrid, ca->gridSize, ca->nGenes
                     );
                     CudaCheckError();
-                    CudaSafeCall(cudaStreamSynchronize(streams[0]));
 
-                    printf("Tumour excision was performed at time step %d.\n",
-                           ticks);
-                    if (ca->exciseCount == ca->maxExcise) {
-                        printf("The maximum number of tumour");
-                        printf(" excisions has been performed.\n");
-                    }
-                    ca->exciseCount++;
+                    excisionPerformed = true;
                     ca->timeTCAlive = 0;
-                    ca->timeTCDead = 1;
+                }
+
+                if (excisionPerformed) {
+                    printf("Excision was performed at time step %d.\n",
+                           ticks);
+                    ca->exciseCount++;
                 }
             }
+            CudaSafeCall(cudaFree(numTC)); numTC = NULL;
+
+            save_cell_data_to_file(ca, ticks, blocks, threads, streams[0]);
 
             reset_rule_params<<< blocks, threads, 0, streams[0] >>>(
                 ca->prevGrid, ca->newGrid, ca->gridSize
@@ -102,38 +180,62 @@ void anim_gpu_ca(uchar4* outputBitmap, unsigned dim, CA *ca,
 
             if ((display || ca->save)
              && ticks % ca->framerate == 0) {
-                display_ca<<< blocks, threads, 0, stream >>>(
+                display_ca<<< blocks, threads, 0, streams[0] >>>(
                     outputBitmap, ca->newGrid, ca->gridSize,
                     ca->cellSize, dim, ca->stateColors);
                 CudaCheckError();
             }
-
-            for (i = 0; i < ca->nCarcin+2; i++)
-                CudaSafeCall(cudaStreamSynchronize(streams[i]));
-
-            printf("Empty: %d, Prolif: %d, Quies: %d, Apop: %d, Diff: %d, ",
-                   count[0], count[1], count[2], count[3], count[4]);
-            printf("Nothing: %d, without empty neigh: %d, ",
-                   count[5], count[6]);
-            printf("TC: %d, TC no action: %d, TC impossible action: %d, TC success: %d, TC died: %d\n",
-                   count[7], count[8], count[9], count[10], count[11]);
-            CudaSafeCall(cudaFree(count)); count = NULL;
         }
 
-        CudaSafeCall(cudaStreamSynchronize(stream));
-        if (ca->save && (!paused || (paused && windowsShouldClose))
+        if (!paused) {
+            CudaSafeCall(cudaMallocManaged((void**)&countData,
+                                           (ca->nStates+ca->nStates*ca->nGenes)*sizeof(unsigned)));
+            memset(countData, 0,
+                   (ca->nStates+ca->nStates*ca->nGenes)*sizeof(unsigned));
+            collect_data<<< blocks, threads, 0, streams[0] >>>(
+                ca->newGrid, countData, ca->gridSize, ca->nGenes
+            );
+            CudaCheckError();
+            CudaSafeCall(cudaStreamSynchronize(streams[0]));
+            for (i = 0; i < ca->nStates; i++) {
+                save_data(ca->countFiles[i], ca->headerCount, ticks,
+                          countData[i], ca->stateColors[i].x,
+                          ca->stateColors[i].y, ca->stateColors[i].z);
+                for (j = 0; j < ca->nGenes; j++) {
+                    if (i == EMPTY) continue;
+                    save_data(ca->countFiles[i*ca->nGenes+(3*ca->nStates+ca->nGenes+11)+j],
+                              ca->headerCount, ticks,
+                              countData[i*ca->nGenes+(ca->nGenes+ca->nStates)+j],
+                              ca->stateColors[i].x, ca->stateColors[i].y,
+                              ca->stateColors[i].z);
+               }
+            }
+            for (i = 0; i < ca->nGenes; i++)
+                save_data(ca->countFiles[i+11], ca->headerCount, ticks,
+                          countData[i+7], ca->geneColors[i].x,
+                          ca->geneColors[i].y, ca->geneColors[i].z);
+
+            numCells = ca->gridSize * ca->gridSize;
+            if (countData[EMPTY] == numCells
+             || countData[CSC] + countData[TC] + countData[EMPTY] == numCells)
+                *windowsShouldClose = true;
+            CudaSafeCall(cudaFree(countData)); countData = NULL;
+        }
+
+        for (i = 0; i < ca->nCarcin+2; i++) {
+            CudaSafeCall(cudaStreamSynchronize(streams[i]));
+            CudaSafeCall(cudaStreamDestroy(streams[i]));
+        }
+
+        if (ca->save && !paused
          && ticks % ca->framerate == 0)
             save_image(outputBitmap, dim, ca->blockSize, NULL, ticks,
                        ca->maxT, ca->devId2);
     }
-
-    for (i = 0; i < ca->nCarcin+2; i++)
-        CudaSafeCall(cudaStreamDestroy(streams[i]));
 }
 
 void anim_gpu_genes(uchar4* outputBitmap, unsigned dim, CA *ca,
-                    unsigned ticks, bool display, bool paused,
-                    bool windowsShouldClose, cudaStream_t stream)
+                    unsigned ticks, bool display, bool paused)
 {
     dim3 blocks(NBLOCKS(ca->gridSize, ca->blockSize),
                 NBLOCKS(ca->gridSize, ca->blockSize));
@@ -141,16 +243,15 @@ void anim_gpu_genes(uchar4* outputBitmap, unsigned dim, CA *ca,
 
     if ((display || ca->save)
      && ticks % ca->framerate == 0) {
-        display_genes<<< blocks, threads, 0, stream >>>(
+        display_genes<<< blocks, threads >>>(
             outputBitmap, ca->newGrid, ca->gridSize, ca->cellSize, dim,
             ca->NN->nOut, ca->geneColors
         );
         CudaCheckError();
     }
 
-    CudaSafeCall(cudaStreamSynchronize(stream));
-    if (ca->save && ((ticks <= ca->maxT && !paused)
-                  || (paused && windowsShouldClose))
+    CudaSafeCall(cudaDeviceSynchronize());
+    if (ca->save && ticks <= ca->maxT && !paused
      && ticks % ca->framerate == 0)
         save_image(outputBitmap, dim, ca->blockSize,
                    ca->prefixes[0], ticks, ca->maxT, ca->devId2);
@@ -158,8 +259,7 @@ void anim_gpu_genes(uchar4* outputBitmap, unsigned dim, CA *ca,
 
 void anim_gpu_carcin(uchar4* outputBitmap, unsigned dim, CA *ca,
                      unsigned carcinIdx, unsigned ticks,
-                     bool display, bool paused, bool windowsShouldClose,
-                     cudaStream_t stream)
+                     bool display, bool paused)
 {
     dim3 blocks(NBLOCKS(ca->gridSize, ca->blockSize),
                 NBLOCKS(ca->gridSize, ca->blockSize));
@@ -167,16 +267,15 @@ void anim_gpu_carcin(uchar4* outputBitmap, unsigned dim, CA *ca,
 
     if ((display || ca->save)
      && ticks % ca->framerate == 0) {
-        display_carcin<<< blocks, threads, 0, stream >>>(
+        display_carcin<<< blocks, threads >>>(
             outputBitmap, &ca->pdes[carcinIdx],
             ca->gridSize, ca->cellSize, dim
         );
         CudaCheckError();
     }
 
-    CudaSafeCall(cudaStreamSynchronize(stream));
-    if (ca->save && ((ticks <= ca->maxT && !paused)
-                  || (paused && windowsShouldClose))
+    CudaSafeCall(cudaDeviceSynchronize());
+    if (ca->save && ticks <= ca->maxT && !paused
      && ticks % ca->framerate == 0)
         save_image(outputBitmap, dim, ca->blockSize,
                    ca->prefixes[carcinIdx+1], ticks, ca->maxT, ca->devId2);
@@ -184,40 +283,41 @@ void anim_gpu_carcin(uchar4* outputBitmap, unsigned dim, CA *ca,
 }
 
 void anim_gpu_cell(uchar4* outputBitmap, unsigned dim, CA *ca,
-                   unsigned cellIdx, unsigned ticks, bool display,
-                   cudaStream_t stream)
+                   unsigned cellIdx, unsigned ticks, bool display)
 {
     dim3 blocks(NBLOCKS(dim, ca->blockSize), NBLOCKS(dim, ca->blockSize));
     dim3 threads(ca->blockSize, ca->blockSize);
 
     if ((display || ca->save)
      && ticks % ca->framerate == 0) {
-        display_cell_data<<< blocks, threads, 0, stream >>>(
+        display_cell_data<<< blocks, threads >>>(
             outputBitmap, ca->newGrid, cellIdx, dim, ca->NN->nOut,
             ca->stateColors
         );
         CudaCheckError();
     }
+
+    CudaSafeCall(cudaDeviceSynchronize());
 }
 
-void anim_gpu_timer_and_saver(CA *ca, bool start, unsigned ticks,
-                              bool paused, bool windowsShouldClose)
+void anim_gpu_timer_and_saver(CA *ca, bool start, unsigned ticks, bool paused,
+                              bool windowsShouldClose)
 {
     const unsigned videoFramerate = 24;
     unsigned carcinIdx;
 
-    if (start) {
+    if (start && !windowsShouldClose) {
         ca->startStep = clock();
         printf("starting %d\n", ticks);
-    } else {
+    } else if (!start) {
         ca->endStep = clock();
         printf("%d took %f seconds to complete.\n", ticks,
                (double) (ca->endStep - ca->startStep) / CLOCKS_PER_SEC);
     }
-    if (ticks == 0) ca->start = clock();
+    if (ticks == 0 && !paused) ca->start = clock();
 
-    if (!start & ca->save && ((ticks == ca->maxT && !paused)
-     || (paused && windowsShouldClose))) {
+    if (!start && ca->save && (windowsShouldClose
+    || (!paused && ticks == ca->maxT))) {
         #pragma omp parallel sections num_threads(3)
         {
             #pragma omp section
@@ -245,10 +345,10 @@ void anim_gpu_timer_and_saver(CA *ca, bool start, unsigned ticks,
         }
     }
 
-    if (ticks == ca->maxT && !start) {
+    if (!start && (ticks == ca->maxT || windowsShouldClose)) {
         ca->end = clock();
         printf("It took %f seconds to run the %d time steps.\n",
-               (double) (ca->end - ca->start) / CLOCKS_PER_SEC, ca->maxT);
+               (double) (ca->end - ca->start) / CLOCKS_PER_SEC, ticks);
     }
 }
 
