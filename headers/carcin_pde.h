c1e15ef4162ec667d59317b7f3ff47125a9d7662
Karl Deutscher <karldeutscher@gmail.com>
2020-06-18 19:50:10 -0600

    1. Upon realizing that the pde was being calculated on the wrong spatial scale    0-N cm instead of 0-N*deltaxy cm the scaling had to be adjusted. 2. Also cellVolume was being used to convert the concentration to grams but that    defeats the purpose of the concentration amount.

--- headers/carcin_pde.h
+++ headers/carcin_pde.h
@@ -1,25 +1,33 @@
 #ifndef __CARCIN_PDE_H__
 #define __CARCIN_PDE_H__
 
+__global__ void init_pde(double*,double,double,unsigned);
+__global__ void pde_space_step(double*,double*,unsigned,unsigned,unsigned,
+                               double,double,double,double,double,double,
+                               double);
+
 struct CarcinPDE {
     int device;
     unsigned carcinIdx;
     unsigned N;
     unsigned maxIter;
 
-    double ic;
-    double bc;
+    double deltaxy, deltat;
     double diffusion;
-    double influx;
-    double outflux;
+    double ic, bc;
+    double influx, outflux;
 
     double *soln;
+    double *maxVal;
 
     CarcinPDE(int dev, unsigned idx, unsigned spaceSize, double diff,
-              double influxIn, double outfluxIn, double icIn, double bcIn)
+              double influxIn, double outfluxIn, double icIn, double bcIn,
+              double cellVolume, double cellCycleLen)
     {
         device = dev;
         N = spaceSize;
+        deltaxy = pow(cellVolume, 1.0/3.0);
+        deltat = cellCycleLen;
         diffusion = diff;
         influx = influxIn;
         outflux = outfluxIn;
@@ -29,6 +37,8 @@ struct CarcinPDE {
         maxIter = 100;
 
         CudaSafeCall(cudaMallocManaged((void**)&soln, N*N*sizeof(double)));
+        CudaSafeCall(cudaMallocManaged((void**)&maxVal, sizeof(double)));
+        *maxVal = ic > bc ? ic : bc;
     }
 
     void prefetch_memory(int dev)
@@ -40,12 +50,12 @@ struct CarcinPDE {
                                           dev, NULL));
     }
 
-    void init(double cellVolume, unsigned blockSize)
+    void init(unsigned blockSize)
     {
         dim3 blocks(NBLOCKS(N, blockSize), NBLOCKS(N, blockSize));
         dim3 threads(blockSize, blockSize);
 
-        init_pde<<< blocks, threads >>>(soln, ic, bc, N, cellVolume);
+        init_pde<<< blocks, threads >>>(soln, ic, bc, N);
         CudaCheckError();
         CudaSafeCall(cudaDeviceSynchronize());
 	}
@@ -57,15 +67,15 @@ struct CarcinPDE {
         }
     }
 
-    void time_step(unsigned step, double cellVolume, unsigned blockSize,
-                   cudaStream_t stream)
+    void time_step(unsigned step, unsigned blockSize,
+                   cudaStream_t *stream)
     {
         dim3 blocks(NBLOCKS(N, blockSize), NBLOCKS(N, blockSize));
         dim3 threads(blockSize, blockSize);
 
-        pde_space_step<<< blocks, threads, 0, stream >>>(
-            soln, step, N, maxIter, bc, ic,
-            diffusion, influx, outflux, cellVolume
+        pde_space_step<<< blocks, threads, 0, *stream >>>(
+            soln, maxVal, step, N, maxIter, bc, ic,
+            diffusion, influx, outflux, deltaxy, deltat
         );
         CudaCheckError();
     }
