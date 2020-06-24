9cf916ff09b1515de5143fb8807bcf8e8bea8aa8
Karl Deutscher <karldeutscher@gmail.com>
2020-06-24 09:24:07 -0600

    Increased the proliferation rates of SC and MSC.

--- headers/cell.h
+++ headers/cell.h
@@ -30,14 +30,14 @@ struct CellParams {
         if (phenoinit == NULL) {
             phenoInit[ NC * nPheno + PROLIF] = 0.024000;
             phenoInit[MNC * nPheno + PROLIF] = 0.024000;
-            phenoInit[ SC * nPheno + PROLIF] = 0.013500;
-            phenoInit[MSC * nPheno + PROLIF] = 0.006750;
+            phenoInit[ SC * nPheno + PROLIF] = 0.013700;
+            phenoInit[MSC * nPheno + PROLIF] = 0.006850;
             phenoInit[CSC * nPheno + PROLIF] = 0.001250;
             phenoInit[ TC * nPheno + PROLIF] = 0.001250;
             phenoInit[ NC * nPheno +  QUIES] = 0.966000;
             phenoInit[MNC * nPheno +  QUIES] = 0.971000;
-            phenoInit[ SC * nPheno +  QUIES] = 0.956500;
-            phenoInit[MSC * nPheno +  QUIES] = 0.965750;
+            phenoInit[ SC * nPheno +  QUIES] = 0.956300;
+            phenoInit[MSC * nPheno +  QUIES] = 0.965650;
             phenoInit[CSC * nPheno +  QUIES] = 0.991250;
             phenoInit[ TC * nPheno +  QUIES] = 0.997500;
             phenoInit[ NC * nPheno +   APOP] = 0.010000;
