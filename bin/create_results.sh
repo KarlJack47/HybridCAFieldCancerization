fd4ac4ea61b699c1eac19f6f07722d6d4615f973
Karl Deutscher <karldeutscher@gmail.com>
2020-06-25 15:11:16 -0600

    1. Changed indentation to 2 spaces from 4. 2. Fixed issue where all out.txt was being used for each individual simulation,    instead of just the section pertaining to that simulation.

--- bin/create_results.sh
+++ bin/create_results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ ! -f main ]; then
-    echo 'The program is not within the current directory.'
-    exit 1
+  echo 'The program is not within the current directory.'
+  exit 1
 fi
 
 help=0
@@ -10,50 +10,50 @@ verbose=0
 numSim=10
 options=()
 while getopts hvdsn:t:g:i:c:a:b:x:pe: option; do
-    case "${option}" in
-        h) help=1;;
-        v) verbose=1;;
-        d) options+=("-d");;
-        s) options+=("-s");;
-	    n) options+=("-n ${OPTARG}")
-           numSim=${OPTARG}
-	       ;;
-        t) options+=("-t ${OPTARG}");;
-        g) options+=("-g ${OPTARG}");;
-        i) options+=("-i ${OPTARG}");;
-        c) options+=("-c ${OPTARG}");;
-        a) options+=("-a ${OPTARG}");;
-        b) options+=("-b ${OPTARG}");;
-        x) options+=("-x ${OPTARG}");;
-        p) options+=("-p");;
-	    e) options+=("-e ${OPTARG}");;
-    esac
+  case "${option}" in
+    h) help=1;;
+    v) verbose=1;;
+    d) options+=("-d");;
+    s) options+=("-s");;
+	n) options+=("-n ${OPTARG}")
+       numSim=${OPTARG}
+	   ;;
+    t) options+=("-t ${OPTARG}");;
+    g) options+=("-g ${OPTARG}");;
+    i) options+=("-i ${OPTARG}");;
+    c) options+=("-c ${OPTARG}");;
+    a) options+=("-a ${OPTARG}");;
+    b) options+=("-b ${OPTARG}");;
+    x) options+=("-x ${OPTARG}");;
+    p) options+=("-p");;
+    e) options+=("-e ${OPTARG}");;
+  esac
 done
 
 if [ $help -eq 1 ]; then
-    echo 'Usage:' $0 '[OPTION]'
-    echo 'Run CA a certain # of times with a set grid size and # of time steps.'
-    echo ''
-    echo 'Options:'
-    echo '  -h     prints out the information you are currently reading'
-    echo '  -v     flag used to display progress of the script, default=disabled'
-    echo '  -d     enables the gui, default=disabled'
-    echo '  -s     enables saving of pictures and videos, default=disabled'
-    echo '  -n int number of simulations to run, default=10'
-    echo '  -t int number of time steps, default=8766'
-    echo '  -g int grid size, default=256 (power of 2 between 16 and 1024)'
-    echo '  -i int init type, default=0, 0=one carcinogen,'
-    echo '         1=start with tumour cells, 2=two carcinogen, 3=no carcinogen'
-    echo '  -p     enables perfect tumour excision, default=disabled'
-    echo '  -e int frequency of tumour excision, default=-1 meaning no excision'
-    echo '         is performed unless excision mode is activated in the gui'
-    exit 0
+  echo 'Usage:' $0 '[OPTION]'
+  echo 'Run CA a certain # of times with a set grid size and # of time steps.'
+  echo ''
+  echo 'Options:'
+  echo '  -h     prints out the information you are currently reading'
+  echo '  -v     flag used to display progress of the script, default=disabled'
+  echo '  -d     enables the gui, default=disabled'
+  echo '  -s     enables saving of pictures and videos, default=disabled'
+  echo '  -n int number of simulations to run, default=10'
+  echo '  -t int number of time steps, default=8766'
+  echo '  -g int grid size, default=256 (power of 2 between 16 and 1024)'
+  echo '  -i int init type, default=0, 0=one carcinogen,'
+  echo '         1=start with tumour cells, 2=two carcinogen, 3=no carcinogen'
+  echo '  -p     enables perfect tumour excision, default=disabled'
+  echo '  -e int frequency of tumour excision, default=-1 meaning no excision'
+  echo '         is performed unless excision mode is activated in the gui'
+  exit 0
 fi
 
 if [ $verbose -eq 1 ]; then
-    echo 'Simulation Runner'
-    echo 'Running' $numSim 'simulations.'
-    echo ''
+  echo 'Simulation Runner'
+  echo 'Running' $numSim 'simulations.'
+  echo ''
 fi
 
 outFolder=output_$(date -d "today" +"%Y%m%d%H%M%S")
@@ -62,37 +62,39 @@ cd $outFolder
 options+=("-f $outFolder")
 
 if [ $verbose -eq 1 ]; then
-    ../main "${options[@]}" >> >(tee out.txt) 2> out.log
+  ../main "${options[@]}" >> >(tee out.txt) 2> out.log
 else
-    ../main "${options[@]}" > out.txt 2> out.log
+  ../main "${options[@]}" > out.txt 2> out.log
 fi
 
 if [ ! -s out.log ]; then
-    rm out.log
+  rm out.log
 fi
 sed -i '/progress/d' out.txt
 
+pattern=('^Starting simulation ' '^Done simulation ')
 for ((i=1; i <= numSim; i++)); do
-    cd $i
-    gnuplot ../../bin/create_plots
-    sed 's/.*Starting simulation $i\(.*\)Done simulation $i/\1/' ../out.txt > $i.txt
-    cd ..
+  cd $i
+  gnuplot ../../bin/create_plots
+  sed -n '/'"${pattern[0]}"$i'/,/'"${pattern[1]}"$i'/p;/'"${pattern[1]}"$i'/q'\
+    ../out.txt > $i.txt
+  cd ..
 done
 
 if [ $verbose -eq 1 ]; then
-    echo ''
+  echo ''
 fi
 
 cd ..
 if [ $verbose -eq 1 ]; then
-    bin/get_stats.sh $outFolder > >(tee $outFolder/stats.txt)\
+  bin/get_stats.sh $outFolder > >(tee $outFolder/stats.txt)\
     2> $outFolder/stats.log
 else
-    bin/get_stats.sh $outFolder > $outFolder/stats.txt 2> $outFolder/stats.log
+  bin/get_stats.sh $outFolder > $outFolder/stats.txt 2> $outFolder/stats.log
 fi
 
 if [ ! -s $outFolder/stats.log ]; then
-    rm $outFolder/stats.log
+  rm $outFolder/stats.log
 fi
 
 exit 0
